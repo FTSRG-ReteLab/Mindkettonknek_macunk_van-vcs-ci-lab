79388e442c35161e7ad68de91be5edef2d4e6a51
Kovacs-Egri Kristof <kovacsegrkristof@gmail.com>
2018-02-19 06:48:19 -0800

    Test file fixed

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TrainSensorTest {
 
     @Before
